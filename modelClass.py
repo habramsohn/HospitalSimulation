a604e949555ae692627e10b1b9d2497e65ffe060
Harrison Abramsohn <hmabramsohn@wm.edu>
2026-03-22 17:28:25 -0400

    further refactor

--- modelClass.py
+++ modelClass.py
@@ -32,25 +32,6 @@ class Personality:
         self.name = name
         self.score = 0
     
-    async def chat(self, message):
-        if not hasattr(self, 'session'):
-            self.session = self.client.aio.chats.create(
-                model="gemini-3-flash-preview",
-                config={'system_instruction': self.prompt}
-            )
-
-        response = await self.session.send_message(message)
-    
-        if response.text == "DONE":
-            print("admin note: done")
-            response = await self.session.send_message("EXIT")
-            right = int(response.text.split("|")[0])
-            wrong = int(response.text.split("|")[1])
-            self.score = right - wrong
-            del self.session
-        
-        return response.text
-    
 # # old  
 # def promptBuild(self, context, name, occupation, personality, knowledge, bias, triggers, constrained, convoLimit):
 #         prompt = f"""

4f4eb931f45e989c2893ca34c5f3dc1d511a5377
Harrison Abramsohn <hmabramsohn@wm.edu>
2026-03-27 10:16:11 -0400

    added error handling to gemini

--- gemini/model.py
+++ gemini/model.py
@@ -10,20 +10,28 @@ class Agent(Personality):
         self.prompt = None
     
     async def chat(self, message):
-        if not hasattr(self, 'session'):
-            self.session = self.client.aio.chats.create(
-                model="gemini-3-flash-preview",
-                config={'system_instruction': self.prompt}
-            )
+        working = 1
+        while working < 1:
+            try:
+                if not hasattr(self, 'session'):
+                    self.session = self.client.aio.chats.create(
+                        model="gemini-3.1-flash-lite-preview",
+                        config={'system_instruction': self.prompt}
+                    )
 
-        response = await self.session.send_message(message)
-    
-        if response.text == "DONE":
-            print("admin note: done")
-            response = await self.session.send_message("EXIT")
-            right = int(response.text.split("|")[0])
-            wrong = int(response.text.split("|")[1])
-            self.score = right - wrong
-            del self.session
+                response = await self.session.send_message(message)
+            
+                if response.text == "DONE":
+                    print("admin note: done")
+                    response = await self.session.send_message("EXIT")
+                    right = int(response.text.split("|")[0])
+                    wrong = int(response.text.split("|")[1])
+                    self.score = right - wrong
+                    del self.session
+                working = 1
+                    
+            except Exception as e:
+                print(f"{e}")
+                working = 0
         
         return response.text

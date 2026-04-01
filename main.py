ac05d574a87a4e8786c816c3a603a554445dd303
Harrison Abramsohn <hmabramsohn@wm.edu>
2026-04-01 18:04:48 -0400

    initialize gemini only branch

--- main.py
+++ main.py
@@ -9,12 +9,6 @@ from llama import main
 load_dotenv()
 api_key = os.getenv('KEY')
 
-choice = input("Select a model: \n 1 - Gemini \n 2 - Ollama \n")
-
 if __name__ == "__main__":
-    if choice == "1":
-        agents = g.main.personality_init(personalities, context, api_key)
-        g.main.run(agents)
-    elif choice == "2": 
-        agents = l.main.personality_init(personalities, context)
-        l.main.run(agents)
+    agents = g.main.personality_init(personalities, context, api_key)
+    g.main.run(agents)

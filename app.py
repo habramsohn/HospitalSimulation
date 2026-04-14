d423456002ec7c0ca946f4ff38a90399f2f236a4
Harrison Abramsohn <hmabramsohn@wm.edu>
2026-04-14 12:00:41 -0400

    prod ready

--- app.py
+++ app.py
@@ -67,6 +67,6 @@ def finished(request: Request):
 
 if __name__ == "__main__":
     #print(agents)
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
-    # port = int(os.environ.get("PORT", 10000))
-    # uvicorn.run("app:app", host="0.0.0.0", port=port)
+    #uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
